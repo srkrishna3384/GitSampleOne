7d4a9f3f634be173d0ec6ddf6c4b0b12515a461c
RK <RK@DESKTOP-4334PHR.testingxperts.loc>
2020-05-08 08:41:57 +0530

    Removed commentlines

--- src/test/java/com/sample/gitprojet/GitSample/GitTest.java
+++ src/test/java/com/sample/gitprojet/GitSample/GitTest.java
@@ -3,11 +3,8 @@ package com.sample.gitprojet.GitSample;
 public class GitTest {
 	public static void main(String[] args) {
 
-		System.out.println("Person2");
-		System.out.println("Person2 code....");
-/*=======
-		System.out.println("Person1");
->>>>>>> f4908c51f37da916ffebeb5dc4b74bc93e96e4a2*/
+		System.out.println("Person11111111");
+		System.out.println("Person1111111111 code....");
 		
 		
 	}
