f28d8dd78322301838957cf657633786c853eda6
RK <RK@DESKTOP-4334PHR.testingxperts.loc>
2020-05-08 08:26:05 +0530

    Updated by person2

--- src/test/java/com/sample/gitprojet/GitSample/GitTest.java
+++ src/test/java/com/sample/gitprojet/GitSample/GitTest.java
@@ -3,6 +3,7 @@ package com.sample.gitprojet.GitSample;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Person2");
+		System.out.println("Person2 code....");
 		
 		
 	}
